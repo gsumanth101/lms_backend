7b890f31960eb619ee3431400f14b8849c7eefd5
GSaiPhanindraPavanKumar <saiphanindra2004@gmail.com>
2024-10-12 20:29:58 +0530

    course

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,18 +1,29 @@
+// lms_backend-main/routes/adminRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const Admin = require('../controllers/adminController');
 const protect = require('../middleware/adminAuthMiddleware');
-const University = require('../models/university');
+const multer = require('multer');
+const path = require('path');
+
+const upload = multer({ dest: 'uploads/' });
 
 router.post('/register', Admin.adminRegister);
 router.post('/login', Admin.adminLogin);
 
 router.get('/profile', protect, Admin.getAdminProfile);
 router.post('/add_org', protect, Admin.createUniversity);
-router.get('/org',  Admin.getUniversities);
-router.get('/org_students',  Admin.getStudentsByUniversity);
+router.get('/org', Admin.getUniversities);
+router.get('/org_students', Admin.getStudentsByUniversity);
 
 router.post('/add_course', protect, Admin.createCourse);
+router.get('/courses', protect, Admin.getCourses); // Get list of courses
+router.get('/courses/:id', protect, Admin.getCourseById);
+router.post('/courses/:id/add-unit', protect, upload.single('scormFile'), Admin.addUnitToCourse); // Add unit to course
+router.post('/courses/:id/assign', protect, Admin.assignCourseToUniversity); // Assign course to university
+router.get('/courses/:courseId/units/:unitId', protect, Admin.viewUnit);
+
 router.post('/upload-users', protect, Admin.bulkUploadStudents);
 router.post('/create-student', protect, Admin.createStudent);
 router.post('/create-spoc', protect, Admin.createSpoc);
@@ -25,17 +36,6 @@ router.get('/spoc_count', Admin.getSpocCount);
 router.get('/faculty_count', Admin.getFacultyCount);
 
 router.get('/spocs', Admin.getAllSpocs);
-router.get('/profile',protect,Admin.getAdminProfile);
-
-// router.get('/dashboard', Admin.getDashboardData);
-
-
-
-
-// router.use(protect);
-
-
-
-
+router.get('/profile', protect, Admin.getAdminProfile);
 
 module.exports = router;
